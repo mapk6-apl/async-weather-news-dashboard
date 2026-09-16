3501a8735849612b8986193601978bc2e9174475
Mmaphuti Kgomo <mmaphutikgomo6@icloud.com>
2026-09-16 20:34:36 +0200

    Created news promise

--- src/promiseVersion.ts
+++ src/promiseVersion.ts
@@ -23,3 +23,23 @@ function getWeatherPromise(): Promise<string>{
         })
     })
 }
+
+function getNewsPromise(): Promise<string>{
+    return new Promise((resolve, reject) => {
+        const url = `https://gnews.io/api/v4/top-headlines?token=${newsKey}&lang=en&max=5`
+
+        https.get(url, (res) => {
+            let data = ''
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            })
+
+            res.on('end', () => {
+                resolve(data);
+            }).on('error', (err) => {
+                reject(err);
+            })
+        })
+    })
+}
